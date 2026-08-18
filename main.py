e7043f1ce9c95a311a7eb427958c09f061c50831
zxcBanya <ztv.danil.r.com>
2026-08-18 16:33:31 +0800

    changed port to 80 for AWS Load Balancer

--- main.py
+++ main.py
@@ -175,4 +175,4 @@ def internal_server_error(e):
     return jsonify({"error": "Internal Server Error. Our maintenance team is on it!"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
